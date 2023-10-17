17281d28f750a78decb321d58ddabad8391a8863
Grzegorz Kmita <jirafey@gmail.com>
2023-10-17 17:27:40 +0400

    Add files via upload

--- Semester-3/Algorithms/lab_01.cpp
+++ Semester-3/Algorithms/lab_01.cpp
@@ -1,5 +1,9 @@
-// lab_01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+
+#include <random>
+
+#include <iostream>
+#include "lista.h"
 
 struct osoba
 {
@@ -7,56 +11,34 @@ struct osoba
     char b;
 };
 
-int cmp_int(int a, int b) {
-    return a - b;
-}
-
-//boool operator ==(osoba o1, osoba o2)
-
-int cmp_osoby(osoba o1, osoba o2) {
-    return o1.a - o2.a;
-}
-
 #include <iostream>
 #include "lista.h"
 int main()
 {
-    lista<int> lista_test;
-    //lista_test.search_data(1);
-    lista_test.add_to_head(1);
-    cout << lista_test.search_data(1) << endl;
-    lista_test.add_to_head(2);
-    cout << lista_test.search_data(1) << endl;
-    cout << lista_test.search_data(2) << endl;
-    lista_test.add_to_head(3);
-    lista_test.add_to_head(4);
-    lista_test.add_to_head(5);
-    lista_test.add_to_tail(6);
-    lista_test.add_to_tail(7);
-    lista_test.add_to_tail(8);
-    lista_test.add_to_tail(9);
-    lista_test.add_to_tail(10);
-    cout << lista_test << endl;
-    lista<osoba> l2;
-    osoba pattern;
-    pattern.a = 13;
+    cout << "START" << endl;
+    std::random_device rd;
+    std::mt19937 mt(rd());
 
-    cout<< lista_test.search_data(9)<<endl;
-    cout << lista_test.to_string(-1) << endl;
-    lista_test.clear();
-    cout << lista_test.to_string(-1) << endl;
-
+    std::uniform_int_distribution<int> distrubution(0, 99);
+    int randomNumber = distrubution(mt);
+    const int MAX_ORDER = 7; // maksymalny rzad wielkosci rozmiaru dodawanych danych
+    lista <int*>* ll = new lista < int* >(); // stworzenie listy
+    for (int o = 1; o <= MAX_ORDER; o++) // petla po kolejnych rzedach wielkosci
+    {
+        const int n = pow(10, o); // rozmiar danych
+        // dodawanie do listy
+        clock_t t1 = clock();
+        for (int i = 0; i < n; i++) {
+            int randomNumber = rand() % 1000; //distrubution(mt);
+            int* pointerToInt = &randomNumber;
+            ll ->add_to_tail(pointerToInt);
+        }
+        clock_t t2 = clock();
+        cout << o << endl;
+        cout << "Czas: " << (t2 - t1) / (double)CLOCKS_PER_SEC << endl;
+    }
+    
+    delete ll;
+    cout << "END" << endl;
+    return 0;
 }
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

92b2f367b8e02f53ed2d2abeacb84d74a3cac837
Grzegorz Kmita <grzegorzkmita@protonmail.com>
2023-12-18 00:12:23 +0100

    Update 14.c

--- Semester-3/Computer Organization & Assembly Language/14.c
+++ Semester-3/Computer Organization & Assembly Language/14.c
@@ -2,74 +2,78 @@
 // Ex. 14
 #include <stdio.h>;
 
-int main(){
-typedef union {
-    unsigned char BYTE;
-    struct {
-        unsigned char a : 1;
-        unsigned char b : 3;
-        unsigned char c : 4;
-    }bits;
-}un_K;
+int main() {
+    typedef union {
+        unsigned char BYTE;
+        struct {
+            unsigned char a : 1;
+            unsigned char b : 3;
+            unsigned char c : 4;
+        }bits;
+    }un_K;
 
-un_K m;
-m.bits.a = 1;
-m.bits.b = 5;
-m.bits.c = 9;
+    un_K m;
+    m.bits.a = 1;
+    m.bits.b = 5;
+    m.bits.c = 9;
+    
+    // insert the number on the "1" bits
+    unsigned char a = m.BYTE & 0b00000001; 
+    unsigned char b = (m.BYTE & 0b00001110) >> 1;
+    unsigned char c = (m.BYTE & 0b11110000) >> 4;
+    
+    c = a * b;
+    
+    // Combine the bits into the whole
+    m.BYTE = 0;
+    m.BYTE = m.BYTE | a;
+    m.BYTE = m.BYTE | b << 1;
+    m.BYTE = m.BYTE | c << 4;
 
-unsigned char a = m.BYTE & 0b00000001;
-unsigned char b = (m.BYTE & 0b00001110)>>1; 
-unsigned char c = (m.BYTE & 0b11110000)>>4;
+    printf("14 C: %d\n\n", m.BYTE);
+    
+    // Reset for the assembly solution
+    m.bits.a = 1;
+    m.bits.b = 5;
+    m.bits.c = 9;
 
-c = a * b;
+    __asm {
+        mov al, m.BYTE
+        and al, 0x01 // 0000 0001
 
-m.BYTE = 0;
-m.BYTE = m.BYTE | a;
-m.BYTE = m.BYTE | b<< 1;
-m.BYTE = m.BYTE | c << 4;
+        mov bl, m.BYTE
+        and bl, 0x0E // 0000 1110
+        shr bl, 1
 
-printf("14 C: %d\n\n", m.BYTE);
+        mov cl, m.BYTE
+        and cl, 0xF0 // 1111 0000
+        shr cl, 4
 
-m.bits.a = 1;
-m.bits.b = 5;
-m.bits.c = 9;
+        mov ah, bl // ax = al_bl
+        mov ch, 0 // cx = 0000_0000_cl
+        multi:
+            add ch, al
+            dec ah // (al)
 
-__asm {
-    mov al, m.BYTE
-    and al, 0x01
+            jz end // if ah = 0 -> end
+            jmp multi // loop
+            end :
+        mov cl, ch
 
-    mov bl, m.BYTE
-    and bl, 0x0E
-    shr bl, 1
+            mov dl, 0
 
-    mov cl, m.BYTE
-    and cl, 0xF0
-    shr cl, 4
+            and al, 0x01 // make content 0x01
+            or dl, al
 
-    mov ah, bl
-    mov ch, 0
-    multi:
-        add ch, al
-        dec ah
-        jz end
-        jmp multi
-    end:
-    mov cl, ch
+            and bl, 0x07 // make content 0x07
+            shl bl, 1
+            or dl, bl
 
-    mov dl, 0
+            and cl, 0x0F // make content 0x0F
+            shl cl, 4
+            or dl, cl
+            mov m.BYTE, dl
+    }
 
-    and al, 0x01
-    or dl, al
-
-    and bl, 0x07
-    shl bl, 1
-    or dl, bl
-
-    and cl, 0x0F
-    shl cl, 4
-    or dl, cl
-    mov m.BYTE, dl
-}
-
-printf("14 ASM: %d\n\n", m.BYTE);
+    printf("14 ASM: %d\n\n", m.BYTE);
 }

049c01b3c974c38d6cb38bea69a661dd5ed677ad
Grzegorz Kmita <grzegorzkmita@protonmail.com>
2023-12-17 23:19:23 +0100

    Create 2.c

---
+++ Semester-3/Computer Organization & Assembly Language/2.c
@@ -0,0 +1,16 @@
+#include <iostream>;
+int main() {
+unsigned char e = 0x33;
+unsigned char b;
+__asm {
+    mov al, e
+    cmp al, 0x32
+    jnz endif
+        mov b, 0
+        jmp end
+    endif:
+        mov b, 1
+    end:
+}
+printf("2: %d\n\n", b);
+}

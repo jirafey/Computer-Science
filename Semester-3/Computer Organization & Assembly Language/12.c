d8d5276099e0e82b589841d6d36a5cdf716c9a23
Grzegorz Kmita <grzegorzkmita@protonmail.com>
2023-12-17 23:19:40 +0100

    Create 12.c

---
+++ Semester-3/Computer Organization & Assembly Language/12.c
@@ -0,0 +1,21 @@
+#include <iostream>;
+int main() {
+unsigned int c = 0;
+unsigned int k = 0;
+__asm {
+    mov eax, c
+    mov ebx, 0
+    mov ecx, 0
+    startt:
+        sub ecx, 0x00000008
+        jnc eend
+        add ebx, 1
+        mov ecx, ebx
+        add eax, ecx
+        jmp startt
+    eend:
+        sub eax, ebx
+        mov c, eax
+}
+printf("12: %d\n\n", c);
+}

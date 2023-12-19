62c0c90c933fa9843d4fa8e16e92620edc0f4610
Grzegorz Kmita <grzegorzkmita@protonmail.com>
2023-12-19 14:24:54 +0100

    Update 12.c

--- Semester-3/Computer Organization & Assembly Language/12.c
+++ Semester-3/Computer Organization & Assembly Language/12.c
@@ -1,25 +1,28 @@
 // Author: folate
+// Edited: jirafey
 // Ex. 12
 #include <stdio.h>;
 
 int main() {
-unsigned int c = 0;
-unsigned int k = 0;
-__asm {
-    mov eax, c
-    mov ebx, 0
-    mov ecx, 0
-    startt:
-        sub ecx, 0x00000008
-        jnc eend
-        add ebx, 1
-        mov ecx, ebx
-        add eax, ecx
-        jmp startt
-    eend:
+    int y = 0;
+    for (int i = 0; i < 5; i++) y += i;
+    printf("%d\n", y);
+    unsigned int c = 0;
+    __asm {
+        mov eax, c
+        mov ebx, 0
+        mov ecx, 0
+        startt:
+        sub ecx, 5
+            jnc eend
+            add ebx, 1
+            mov ecx, ebx
+            add eax, ecx
+            jmp startt
+            eend :
         sub eax, ebx
-        mov c, eax
-}
+            mov c, eax
+    }
 
-printf("12: %d\n\n", c);
+    printf("12: %d\n\n", c);
 }

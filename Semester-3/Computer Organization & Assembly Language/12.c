9a99b78be415491eadf097807e09cb72c3a7e1da
Grzegorz Kmita <grzegorzkmita@protonmail.com>
2023-12-19 14:45:24 +0100

    Update 12.c

--- Semester-3/Computer Organization & Assembly Language/12.c
+++ Semester-3/Computer Organization & Assembly Language/12.c
@@ -9,14 +9,14 @@ int main()
     y = 0;
     _asm {
         
-        mov eax, 0;
+        mov eax, 0
     loop_start:
-        sub eax, 5;
-        jnc loop_end;
-        add eax, 5;
-        add y, eax;
-        add eax, 1;
-        jmp loop_start;
+        sub eax, 5
+        jnc loop_end
+        add eax, 5
+        add y, eax
+        add eax, 1
+        jmp loop_start
     loop_end:
     }
     printf("12 asm: %d\n", y);

56efef5bba6271d87f129a2167028350ae7a88a7
jirafey <97115044+jirafey@users.noreply.github.com>
2024-03-06 13:35:13 +0100

    Update lab01-02.py

--- Semester-4/piad/lab01-02.py
+++ Semester-4/piad/lab01-02.py
@@ -63,31 +63,65 @@ if __name__ == '__main__':
     # print(a)
     # print(b)
 
-    print(a+b)
-    print(a-b)
-    print(a*b)
-    print(np.power(a,b))
+    # print(a+b)
+    # print(a-b)
+    # print(a*b)
+    # print(np.power(a,b))
+    #
+    # print(a >=4)
+    #
+    # print('Diagonal (sum): ', np.trace(b))
+    # print(sum(b))
+    # print(np.min(b))
+    # print(np.max(b))
+    # print(np.std(b))
 
-    print(a >=4)
+    a = np.arange(50).reshape((10, 5))
+    # print(a)
+    a = np.arange(50)
+    a = np.resize(a, (10, 5))
+    # print(a)
 
-    print('Diagonal (sum): ', np.trace(b))
-    print(sum(b))
-    print(np.min(b))
-    print(np.max(b))
-    print(np.std(b))
-
+    a = np.array([1, 2, 3, 4, 5])
+    b = np.array([1, 2, 3, 4])
+    # print(a+b)
+    x1_new = a+b[:, np.newaxis]
+    # print(x1_new)
     # Rzutowanie wymiarów za pomocą shape lub resize:
+
+    # sortowanie:
+
     # Tasks
     # 1.
+    b = np.random.randint(0, 100, (10, 5)).astype('int32')
+    # print(b)
+
+    # print('Diagonal (sum): ', np.trace(b))
+    # print(np.diag(b))
+
     # 2.
+    a = rng.standard_normal(10)
+    b = rng.standard_normal(10)
+    # print(a*b)
     # 3.
+    a = np.random.randint(101, size=100)
+    b = np.random.randint(101, size=100)
+
+    print(a)
+    print(b)
+
+    np.asmatrix(a)
+    np.asmatrix(b)
+
+    a = np.reshape(a, (20,5))
+    b = np.reshape(b, (20,5))
+    # 
+    # print("a:\n",a, "\n")
+    # print("b:\n",b, "\n")
     # 4.
+    
     # 5.
     # 6.
     # 7.
     # 8.
     # 9.
-# Press the green button in the gutter to run the script.
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
